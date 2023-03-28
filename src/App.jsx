f2944b6a3c089e086c3e5b1a056eab54cbc3c414
androidblog <androidblog@yandex.ru>
2023-03-28 19:35:36 +0400

    create styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,54 @@ import { useState } from "react";
 function App() {
   const [bill, setBill] = useState(0);
   const [tip, setTip] = useState(0);
+  const [customTip, setCustomTip] = useState(0);
   const [people, setPeople] = useState(0);
-  const [tipAmount, setTipAmount] = useState(0);
-  const [total, setTotal] = useState(0);
+  const [tipAmount, setTipAmount] = useState(4.27);
+  const [total, setTotal] = useState(32.79);
 
   return (
     <div className="App">
       <div className="logo-container"></div>
       <div className="component-container">
         <div className="tip-inputs-container">
-          <label htmlFor="bill-input">Bill</label>
-          <input
-            type="number"
-            id="bill-input"
-            name="bill-input"
-            placeholder=""
-            step="0.01"
-          />
-          <label htmlFor="tip-input">Select Tip %</label>
-          <div className="tip-buttons-container">
-            {["5%", "10%", "15%", "25%", "50%"].map((tip) => {
-              return (
-                <button
-                  key={tip}
-                  name="tip-button"
-                  id={tip}
-                  className="tip-button"
-                >
-                  {tip}
-                </button>
-              );
-            })}
+          <div className="bill-input-block">
+            <label htmlFor="bill-input">Bill</label>
             <input
               type="number"
-              step="0.5"
-              placeholder="Custom"
-              id="manual-tip-input"
-              className="manual-tip"
+              id="bill-input"
+              name="bill-input"
+              placeholder=""
+              step="0.01"
             />
           </div>
-          <label htmlFor="people-number">Number of people</label>
-          <input type="number" id="people-number" name="people-number" />
+          <div className="tip-select-block">
+            <label htmlFor="tip-input">Select Tip %</label>
+            <div className="tip-buttons-container">
+              {["5%", "10%", "15%", "25%", "50%"].map((tip) => {
+                return (
+                  <button
+                    key={tip}
+                    name="tip-button"
+                    id={tip}
+                    className="tip-button"
+                  >
+                    {tip}
+                  </button>
+                );
+              })}
+              <input
+                type="number"
+                step="0.5"
+                placeholder="Custom"
+                id="manual-tip-input"
+                className="manual-tip"
+              />
+            </div>
+          </div>
+          <div className="people-number-block">
+            <label htmlFor="people-number">Number of people</label>
+            <input type="number" id="people-number" name="people-number" />
+          </div>
         </div>
         <div className="tip-outputs-container">
           <div className="tip-result-row">
