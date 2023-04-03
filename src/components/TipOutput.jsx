a19f823f59c0af68e6d410ae91d4eb13656905ce
androidblog <androidblog@yandex.ru>
2023-04-03 14:08:46 +0400

    upd css modules name

--- src/components/TipOutput.jsx
+++ src/components/TipOutput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from "./TipStyles.module.css";
-import buttonStyles from "./ButtonStyles.module.css";
+import styles from "..components/TipStyles.module.css";
+import buttonStyles from "..components/ButtonStyles.module.css";
 
 export default function TipOutput({
   tipAmount,
