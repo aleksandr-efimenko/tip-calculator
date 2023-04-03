50b73f5264af91b2f562a7cf472ed64f82521e1a
androidblog <androidblog@yandex.ru>
2023-04-03 14:13:16 +0400

    move styles to folder

--- src/components/TipOutput.jsx
+++ src/components/TipOutput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from "..components/TipStyles.module.css";
-import buttonStyles from "..components/ButtonStyles.module.css";
+import styles from "..styles/TipStyles.module.css";
+import buttonStyles from "../styles/ButtonStyles.module.css";
 
 export default function TipOutput({
   tipAmount,
